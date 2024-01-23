df36d07d7fcba9a80d8d6f8feea35de901717519
Ponte, Nadia <deponte.work@gmail.com>
2024-01-23 23:19:46 +0000

    Array to store team members

--- index.js
+++ index.js
@@ -13,3 +13,5 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// Array to store team members
+const teamMembers = [];
